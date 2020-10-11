a6987e44cbe49606a070d09116564921a8c630a4
Andrii Kravchenko <andrii.kravchenko@goeuro.com>
2020-10-11 18:46:06 +0200

    Initial Commit

---
+++ OpenG/OpenGApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGApp.swift
+//  OpenG
+//
+//  Created by Andrii Kravchenko on 11.10.20.
+//
+
+import SwiftUI
+
+@main
+struct OpenGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
